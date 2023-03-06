495050d358d70d0c119db88f9998eb8e0dc60bec
Sundus <sundusaden105@gmail.com>
2023-03-06 23:00:36 +0300

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding messages..."
 
-# Seed your database here
+
+# 10.times do
+#   title = Faker::Hipster.sentence(word_count: rand(2..4)).chomp('.')
+#   message = Faker::ChuckNorris.fact
+#   user_id = rand(1..10) 
+#   Meme.create(title: title, message: message, user_id: user_id)
+# end
+
+# 10.times do
+#   name = Faker::Name.name
+#   email = Faker::Email.email
+#   sex = Faker::Sex.sex
+#   User.create(name:name, email:email, sex: sex)
+# end
+
+
 
 puts "✅ Done seeding!"
